d021607461e8f16465d2abbfe66650fd6e39ea84
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-02-09 14:04:27 -0200

    feat: add categories reducer

---
+++ frontend/src/reducers/categories.js
@@ -0,0 +1,16 @@
+import {
+  LOAD_CATEGORIES,
+} from '../actions/categories'
+
+const categories = (state = [], action) => {
+  switch (action.type) {
+    case LOAD_CATEGORIES:
+      return [
+        ...action.categories,
+      ]
+    default:
+      return state
+  }
+}
+
+export default categories
