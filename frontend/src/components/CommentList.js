5f01565ca1fb84c86d65ae54e09d96f3ef7a43c8
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-02-25 01:29:00 -0300

    fix: order comments by voteScore
    reference #6

--- frontend/src/components/CommentList.js
+++ frontend/src/components/CommentList.js
@@ -1,13 +1,18 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import {
+  prop,
+  reverse,
+  sortBy,
+} from 'ramda'
+import {
   Typography,
 } from '@material-ui/core'
 import Comment, { commentPropType } from './Comment'
 
 class CommentList extends PureComponent {
   render() {
-    const { comments } = this.props
+    const comments = reverse(sortBy(prop('voteScore'))(this.props.comments))
     return (
       <div>
         <Typography variant="h5" gutterBottom>
